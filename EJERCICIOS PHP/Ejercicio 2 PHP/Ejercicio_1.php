490d6ba9565e13f6f16ba9c6e7b2d410a780acb9
juanmi <jmpecerom03@suarezdefigueroa.es>
2024-12-05 13:39:36 +0100

    PHP

---
+++ EJERCICIOS PHP/Ejercicio 2 PHP/Ejercicio_1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 1 PHP</title>
+</head>
+<body>
+    <?php echo"<p>Hola, ya se imprimir frases</p>"?>
+</body>
+</html>
