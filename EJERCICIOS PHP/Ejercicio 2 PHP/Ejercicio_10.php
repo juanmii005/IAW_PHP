48525003cc02e6aad1c8ab5a7724fcf7bfa8a598
juanmi <jmpecerom03@suarezdefigueroa.es>
2024-12-11 14:14:51 +0100

    PHP

---
+++ EJERCICIOS PHP/Ejercicio 2 PHP/Ejercicio_10.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 8 PHP</title>
+</head>
+<body>
+    <?php
+        $variable = "Daniel Franganillo Vaquero Robles";
+
+        echo $variable;
+    ?>
+</body>
+</html>
---
+++ EJERCICIOS PHP/Ejercicio 2 PHP/Ejercicio_9.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 8 PHP</title>
+</head>
+<body>
+    <?php
+        $nombre = "Daniel";
+        $apellido = "Franganillo";
+        $localidad= "Aceuchal";
+        $provincia = "Badajoz";
+        $nacimiento = "2005";
+
+        echo $nombre." ". $apellido." ".$localidad." ".$provincia." ".$nacimiento;
+
+    ?>
+</body>
+</html>
