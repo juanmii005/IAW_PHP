eebf762702b263e683f36def9e956603accd5b14
juanmi <jmpecerom03@suarezdefigueroa.es>
2024-12-16 10:59:27 +0100

    PHP

--- EJERCICIOS PHP/Ejercicio 2 PHP/Ejercicio_36.php
+++ EJERCICIOS PHP/Ejercicio 2 PHP/Ejercicio_36.php
@@ -7,16 +7,16 @@
 <body>
     <?php 
        echo "Numeros naturales al cuadrado <br>";
-       echo "0 al cuadrado = ".pow(0,2);
-       echo "1 al cuadrado = ".pow(1,2);
-       echo "2 al cuadrado = ".pow(2,2);
-       echo "3 al cuadrado = ".pow(3,2);
-       echo "4 al cuadrado = ".pow(4,2);
-       echo "5 al cuadrado = ".pow(5,2);
-       echo "6 al cuadrado = ".pow(6,2);
-       echo "7 al cuadrado = ".pow(7,2);
-       echo "8 al cuadrado = ".pow(8,2);
-       echo "9 al cuadrado = ".pow(9,2);
+       echo "0 al cuadrado = ".pow(0,2)."<br>";
+       echo "1 al cuadrado = ".pow(1,2)."<br>";
+       echo "2 al cuadrado = ".pow(2,2)."<br>";
+       echo "3 al cuadrado = ".pow(3,2)."<br>";
+       echo "4 al cuadrado = ".pow(4,2)."<br>";
+       echo "5 al cuadrado = ".pow(5,2)."<br>";
+       echo "6 al cuadrado = ".pow(6,2)."<br>";
+       echo "7 al cuadrado = ".pow(7,2)."<br>";
+       echo "8 al cuadrado = ".pow(8,2)."<br>";
+       echo "9 al cuadrado = ".pow(9,2)."<br>";
 
     ?>
 </body>
