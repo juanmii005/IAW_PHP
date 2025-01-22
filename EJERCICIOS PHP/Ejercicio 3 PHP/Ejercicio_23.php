b355834714ee5b2f52d057aa5116250df2d99609
juanmi <jmpecerom03@suarezdefigueroa.es>
2025-01-22 13:54:08 +0100

    PHP

--- EJERCICIOS PHP/Ejercicio 3 PHP/Ejercicio_23.php
+++ EJERCICIOS PHP/Ejercicio 3 PHP/Ejercicio_23.php
@@ -11,14 +11,14 @@
     </form>
 
     <?php
-        $temp = floatval($_GET['temp'] ?? 0);
+        $temp = floatval($_GET['temp'] ?? 32);
         $celsius = (5 / 9 * ($temp - 32));
 
         if ($temp < 999) {
-            echo "$temp grados Fahrenheit son $celsius grados celsius";
+            echo "<br>$temp grados Fahrenheit son $celsius grados celsius";
         }
         else {
-            echo "Programa finalizado";
+            echo "<br>Programa finalizado";
         }       
 
     ?>
