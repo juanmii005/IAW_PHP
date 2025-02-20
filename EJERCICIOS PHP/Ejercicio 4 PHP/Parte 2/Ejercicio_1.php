472738498cd559102bfc9ec9fbdb3c851479bad3
juanmi <jmpecerom03@suarezdefigueroa.es>
2025-02-20 14:08:54 +0100

    PHP

---
+++ EJERCICIOS PHP/Ejercicio 4 PHP/Parte 2/Ejercicio_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 1 PHP</title>
+</head>
+<body>
+    <?php
+        function suma($a, $b) {return $a+$b;}
+        $resultado = suma(10,33);
+        echo "La suma 10 + 33 es igual a $resultado";
+    ?>
+</body>
+</html>
