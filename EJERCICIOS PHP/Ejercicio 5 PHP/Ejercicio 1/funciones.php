f311daa40236bad29da733a8c63d3da92251f2dd
juanmi <jmpecerom03@suarezdefigueroa.es>
2025-03-03 01:40:17 +0100

    PHP

--- EJERCICIOS PHP/Ejercicio 5 PHP/Ejercicio 1/funciones.php
+++ EJERCICIOS PHP/Ejercicio 5 PHP/Ejercicio 1/funciones.php
@@ -2,7 +2,7 @@
 include_once 'conexion.php';
 
 function registro($conn, $dni, $nombre, $apellidos, $localidad, $centro_de_estudios, $usuario, $contraseña) {
-    $consulta = "INSERT INTO usuarios (dni, nombre, apellidos, localidad, centro_de_estudios, usuario, contraseña) VALUES ('$dni', '$nombre', '$apellidos', '$localidad', '$centro_de_estudios', '$usuario', '$contraseña')";
+    $consulta = "INSERT INTO usuarios (dni, nombre, apellidos, localidad, centro_de_estudios, usuario, contraseña) VALUES ('$dni', '$nombre', '$apellidos', '$localidad', '$centro', '$usuario', '$contraseña')";
     $final = mysqli_query($conn, $consulta);
     
     if($final){
